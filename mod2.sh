d2658ae3169b9383a4347c1c37b2304ba8fc4a3a
RAraghavarora <f20171016@pilani.bits-pilani.ac.in>
2021-07-02 10:24:10 +0530

    Changed job file

--- mod2.sh
+++ mod2.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpu:2                      # use 1 GPU per node (i.e. use one GPU per task)
 #SBATCH --nodes=1                        # request 1 node
 #SBATCH --ntasks=8
-#SBATCH -J nnd1-p2
+#SBATCH -J nn-dftb-ra
 #SBATCH --output=dip.out
 #SBATCH --error=dip.err
 #SBATCH -A p_biomolecules
