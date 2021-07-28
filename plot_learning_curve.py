9f9ac8be7da42e72f95227a7e59f010aeedd16a6
RAraghavarora <f20171016@pilani.bits-pilani.ac.in>
2021-07-28 17:50:39 +0530

    Plot Learning Curve: - Edited plot_learning_curve to include conv data - Generated Learning curve comparing the 3 models - Moved all the images to a separate directory

--- plot_learning_curve.py
+++ plot_learning_curve.py
@@ -5,10 +5,12 @@ train_set = [int(i) for i in ['1000', '2000', '4000',
 
 y1 = []  # Only molecular descriptors
 y2 = []  # Mol Desc + DFTB
+y3 = []  # Separate models (without tuner)
 for i in train_set:
     try:
         f1 = open("%s/errors.dat" % i, 'r')
         f2 = open("withdft/%s/errors.dat" % i, 'r')
+        f3 = open("conv/withdft/%s/errors.dat" % i, 'r')
     except:
         print(i)
         continue
@@ -20,11 +22,17 @@ for i in train_set:
     a, b, c = lines2[0].split()
     y2.append(round(float(b), 3))  # Rounded upto 3 decimal places
 
+    lines3 = f3.readlines()
+    a, b, c = lines3[0].split()
+    y3.append(round(float(b), 3))  # Rounded upto 3 decimal places
+
 print(y1)
 print(y2)
+print(y3)
 
 plt.plot(train_set, y1, 'gs-', label='Without dftb')
 plt.plot(train_set, y2, 'bs-', label='With dftb')
+plt.plot(train_set, y3, 'o:', label='Separate models without tuner')
 
 plt.annotate('(%s, %s)' % (30000, y1[-1]),
              xy=(30000, y1[-1]), textcoords='data')
@@ -35,7 +43,6 @@ plt.annotate('(%s, %s)' % (30000, y2[-1]),
              xy=(30000, y2[-1]), textcoords='data')
 plt.annotate('(%s, %s)' % (20000, y2[-2]),
              xy=(20000, y2[-2]), textcoords='data')
-
 
 plt.xlabel('Training size')
 plt.ylabel('MAE (eV)')
