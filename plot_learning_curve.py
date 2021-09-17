ac86103cb36673f34391e29fc7df4b6ef2af2902
RAraghavarora <f20171016@pilani.bits-pilani.ac.in>
2021-09-17 10:30:16 +0530

    Modified plot_learning_curve

--- plot_learning_curve.py
+++ plot_learning_curve.py
@@ -7,17 +7,16 @@ train_set = [
 
 y1 = []  # Only molecular descriptors
 y2 = []  # Mol Desc + DFTB
-y3 = []  # Separate models (without tuner)
+y3 = []  # Separate models
 y4 = []  # Desc + DFTB (with standard)
-y5 = []  # conv2 with standard
+
 
 for i in train_set:
     try:
-        f1 = open("%s/errors.dat" % i, 'r')
-        f2 = open("withdft/%s/errors.dat" % i, 'r')
-        f3 = open("conv/withdft/%s/errors.dat" % i, 'r')
+        f1 = open("%s/errors_test.dat" % i, 'r')
+        f2 = open("withdft/%s/errors_test.dat" % i, 'r')
+        f3 = open("conv2/new2/%s/errors_test.dat" % i, 'r')
         f4 = open("standard/%s/errors.dat" % i, 'r')
-        f5 = open("conv2/new2/%s/errors.dat" % i, 'r')
     except Exception as e:
         print("*******\n")
         print(e)
@@ -39,22 +38,15 @@ for i in train_set:
     a, b, c = lines4[0].split()
     y4.append(round(float(b), 3))  # Rounded upto 3 decimal places
 
-    lines5 = f5.readlines()
-    a, b, c = lines5[0].split()
-    y5.append(round(float(b), 3))  # Rounded upto 3 decimal places
-
-
 print(y1)
 print(y2)
 print(y3)
 print(y4)
-print(y5)
 
 plt.plot(train_set, y1, 's-', label='Without dftb')
 plt.plot(train_set, y2, 's-', label='With dftb')
-plt.plot(train_set, y3, 'o:', label='Separate models without tuner')
+plt.plot(train_set, y3, 'o:', label='Separate models with concatenation')
 plt.plot(train_set, y4, 'o:', label='standard')
-plt.plot(train_set, y5, '.--', label='Separate models with standardization')
 
 plt.annotate('(%s, %s)' % (30000, y1[-1]), xy=(30000, y1[-1]), textcoords='data')
 plt.annotate('(%s, %s)' % (20000, y1[-2]), xy=(20000, y1[-2]), textcoords='data')
@@ -62,8 +54,6 @@ plt.annotate('(%s, %s)' % (20000, y1[-2]), xy=(20000, y1[-2]), textcoords='data'
 plt.annotate('(%s, %s)' % (30000, y2[-1]), xy=(30000, y2[-1]), textcoords='data')
 plt.annotate('(%s, %s)' % (20000, y2[-2]), xy=(20000, y2[-2]), textcoords='data')
 
-plt.annotate('(%s, %s)' % (30000, y5[-1]), xy=(30000, y5[-1]), textcoords='data')
-plt.annotate('(%s, %s)' % (20000, y5[-2]), xy=(20000, y5[-2]), textcoords='data')
 
 plt.xlabel('Training size')
 plt.ylabel('MAE (eV)')
