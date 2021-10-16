bca4223a2cd51224bdd31ef971241ba9811410d9
Raghav Arora <f20171016@pilani.bits-pilani.ac.in>
2021-10-16 15:01:46 +0530

    Use log scale for learning curve

--- plot_learning_curve.py
+++ plot_learning_curve.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 train_set = [
     int(i) for i in ['1000', '2000', '4000', '8000', '10000', '20000', '30000']
@@ -16,7 +17,7 @@ for i in train_set:
         f1 = open("%s/errors_test.dat" % i, 'r')
         f2 = open("withdft/%s/errors_test.dat" % i, 'r')
         f3 = open("conv2/new2/%s/errors_test.dat" % i, 'r')
-        f4 = open("standard/%s/errors.dat" % i, 'r')
+        f4 = open("standard/%s/errors_test.dat" % i, 'r')
     except Exception as e:
         print("*******\n")
         print(e)
@@ -43,16 +44,21 @@ print(y2)
 print(y3)
 print(y4)
 
-plt.plot(train_set, y1, 's-', label='Without dftb')
-plt.plot(train_set, y2, 's-', label='With dftb')
-plt.plot(train_set, y3, 'o:', label='Separate models with concatenation')
-plt.plot(train_set, y4, 'o:', label='standard')
+# plt.yscale("log") 
+plt.grid(True, which="both")
+plt.semilogy(train_set, y1, 's-', label='Architecture 1')
+plt.plot(train_set, y2, 's-', label='Architecture 2 Without standardization')
+# plt.plot(train_set, y3, 'o:', label='Separate models with concatenation')
+plt.plot(train_set, y4, 'o:', label='Architecture 2 With Data Standardization')
 
-plt.annotate('(%s, %s)' % (30000, y1[-1]), xy=(30000, y1[-1]), textcoords='data')
-plt.annotate('(%s, %s)' % (20000, y1[-2]), xy=(20000, y1[-2]), textcoords='data')
+# plt.annotate('(%s, %s)' % (30000, y1[-1]), xy=(30000, y1[-1]), textcoords='data')
+# plt.annotate('(%s, %s)' % (20000, y1[-2]), xy=(20000, y1[-2]), textcoords='data')
 
-plt.annotate('(%s, %s)' % (30000, y2[-1]), xy=(30000, y2[-1]), textcoords='data')
-plt.annotate('(%s, %s)' % (20000, y2[-2]), xy=(20000, y2[-2]), textcoords='data')
+# plt.annotate('(%s, %s)' % (30000, y2[-1]), xy=(30000, y2[-1]), textcoords='data')
+# plt.annotate('(%s, %s)' % (20000, y2[-2]), xy=(20000, y2[-2]), textcoords='data')
+
+# plt.annotate('(%s, %s)' % (30000, y4[-1]), xy=(30000, y4[-1]), textcoords='data')
+
 
 
 plt.xlabel('Training size')
