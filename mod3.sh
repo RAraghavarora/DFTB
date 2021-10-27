012e486432ccf2b6bbaafd915708ecc0864be8ab
RAraghavarora <f20171016@pilani.bits-pilani.ac.in>
2021-10-27 16:40:25 +0530

    Use egap for mod3.sh

--- mod3.sh
+++ mod3.sh
@@ -49,7 +49,7 @@ echo "training starts"
 #export DFTB_PREFIX='/home/medranos/SK-files/3ob-3-1/'
 
 work=/scratch/ws/1/medranos-DFTB/raghav/code
-python3 $work/only_CM.py
+python3 $work/only_CM.py EGAP fit
 
 echo "training is over :-)"
 EXTSTAT=$?
