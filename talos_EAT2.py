23ec775508b80d59e15310fad7e2fb50f69c2fb3
RAraghavarora <f20171016@pilani.bits-pilani.ac.in>
2021-10-28 17:22:00 +0530

    a

--- talos_EAT2.py
+++ talos_EAT2.py
@@ -17,10 +17,10 @@ from tensorflow.keras.callbacks import Callback, ReduceLROnPlateau
 from tensorflow.keras import backend
 from tensorflow.keras.models import load_model
 from tensorflow.keras.initializers import HeNormal
-from qml.representations import generate_coulomb_matrix
+# from qml.representations import generate_coulomb_matrix
 
 import logging
-import schnetpack as spk
+# import schnetpack as spk
 import talos as ta
 
 # monitor the learning rate
@@ -36,163 +36,6 @@ class LearningRateMonitor(Callback):
         # get and store the learning rate
         lrate = float(backend.get_value(self.model.optimizer.lr))
         self.lrates.append(lrate)
-
-
-def complete_array(Aprop):
-    Aprop2 = []
-    for ii in range(len(Aprop)):
-        n1 = len(Aprop[ii])
-        if n1 == 23:
-            Aprop2.append(Aprop[ii])
-        else:
-            n2 = 23 - n1
-            Aprop2.append(np.concatenate((Aprop[ii], np.zeros(n2)), axis=None))
-
-    return Aprop2
-
-
-# prepare train and test dataset
-
-
-def prepare_data(op):
-    # read dataset
-    # data_dir = '../'
-    data_dir = '/scratch/ws/1/medranos-DFTB/raghav/data/'
-    properties = [
-        'RMSD',
-        'EAT',
-        'EMBD',
-        'EGAP',
-        'KSE',
-        'FermiEne',
-        'BandEne',
-        'NumElec',
-        'h0Ene',
-        'sccEne',
-        '3rdEne',
-        'RepEne',
-        'mbdEne',
-        'TBdip',
-        'TBeig',
-        'TBchg',
-    ]
-
-    # data preparation
-    logging.info("get dataset")
-    dataset = spk.data.AtomsData(data_dir + 'totgdb7x_pbe0.db', load_only=properties)
-
-    n = len(dataset)
-    print(n)
-    idx = np.arange(n)
-    np.random.seed(2314)
-    idx2 = np.random.permutation(idx)
-
-    # computing predicted property
-    logging.info("get predicted property")
-    AE, xyz, Z = [], [], []
-    EGAP, KSE, TPROP = [], [], []
-    p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11 = ([] for i in range(11))
-    for i in idx2[:n]:
-        atoms, props = dataset.get_properties(i)
-        AE.append(float(props['EAT']))
-        EGAP.append(float(props['EAT']))
-        KSE.append(props['KSE'])
-        TPROP.append(float(props[op]))
-        xyz.append(atoms.get_positions())
-        Z.append(atoms.get_atomic_numbers())
-        p1.append(float(props['FermiEne']))
-        p2.append(float(props['BandEne']))
-        p3.append(float(props['NumElec']))
-        p4.append(float(props['h0Ene']))
-        p5.append(float(props['sccEne']))
-        p6.append(float(props['3rdEne']))
-        p7.append(float(props['RepEne']))
-        p8.append(float(props['mbdEne']))
-        p9.append(props['TBdip'])
-        p10.append(props['TBeig'])
-        p11.append(props['TBchg'])
-
-    AE = np.array(AE)
-    EGAP = np.array(EGAP)
-    TPROP = np.array(TPROP)
-
-    # Generate representations
-    # Coulomb matrix
-    xyz_reps = np.array(
-        [generate_coulomb_matrix(Z[mol], xyz[mol], sorting='unsorted') for mol in idx2]
-    )
-
-    TPROP2 = []
-    p1b, p2b, p11b, p3b, p4b, p5b, p6b, p7b, p8b, p9b, p10b = (
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-    )
-    for nn in idx2:
-        p1b.append(p1[nn])
-        p2b.append(p2[nn])
-        p3b.append(p3[nn])
-        p4b.append(p4[nn])
-        p5b.append(p5[nn])
-        p6b.append(p6[nn])
-        p7b.append(p7[nn])
-        p8b.append(p8[nn])
-        p9b.append(p9[nn].numpy())
-        p10b.append(p10[nn].numpy())
-        p11b.append(p11[nn].numpy())
-        TPROP2.append(TPROP[nn])
-
-    p11b = complete_array(p11b)
-
-    # Standardize the data property wise
-
-    temp = []
-    for var in [p1b, p2b, p3b, p4b, p5b, p6b, p7b, p8b, p9b, p10b, p11b]:
-        var2 = np.array(var)
-        try:
-            _ = var2.shape[1]
-        except IndexError:
-            var2 = var2.reshape(-1, 1)
-        scaler = StandardScaler()
-        var3 = scaler.fit_transform(var2)
-
-        temp.append(var3)
-
-    p1b, p2b, p3b, p4b, p5b, p6b, p7b, p8b, p9b, p10b, p11b = temp
-
-    reps2 = []
-    for ii in range(len(idx2)):
-        # reps2.append(xyz_reps[ii])
-        reps2.append(
-            np.concatenate(
-                (
-                    xyz_reps[ii],
-                    p1b[ii],
-                    p2b[ii],
-                    p3b[ii],
-                    p4b[ii],
-                    p5b[ii],
-                    p6b[ii],
-                    p7b[ii],
-                    p8b[ii],
-                    np.linalg.norm(p9b[ii]),
-                    p10b[ii],
-                    p11b[ii],
-                ),
-                axis=None,
-            )
-        )
-    reps2 = np.array(reps2)
-
-    return reps2, TPROP2
 
 
 def split_data(n_train, n_val, n_test, Repre, Target):
@@ -384,7 +227,8 @@ n_val = 1000
 n_test = 10000
 op = sys.argv[1]
 
-iX, iY = prepare_data(op)
+iX = np.load('/scratch/ws/1/medranos-DFTB/raghav/data/iX.npy')
+iY = np.load('/scratch/ws/1/medranos-DFTB/raghav/data/iY.npy')
 
 # fit model and plot learning curves for a patience
 patience = 500
