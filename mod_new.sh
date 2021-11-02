08195fc1fb811eec95b30b322f3ea7e796a2f94e
RAraghavarora <f20171016@pilani.bits-pilani.ac.in>
2021-11-02 23:18:06 +0530

    train_new without dftb

--- mod_new.sh
+++ mod_new.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpu:2                      # use 1 GPU per node (i.e. use one GPU per task)
 #SBATCH --nodes=1                        # request 1 node
 #SBATCH --ntasks=8
-#SBATCH -J withdft-new
+#SBATCH -J nodft-new
 #SBATCH --output=withdft/new.out
 #SBATCH --error=withdft/new.err
 #SBATCH -A p_biomolecules
