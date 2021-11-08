fa7b0d4e337069950840d645ee5d7bcd0a06c766
RAraghavarora <f20171016@pilani.bits-pilani.ac.in>
2021-11-08 18:58:18 +0530

    bob-nodft-eq 30k

--- train_dftb.py
+++ train_dftb.py
@@ -364,7 +364,7 @@ def save_plot(n_val):
 
 
 # prepare dataset
-train_set = ['1000', '2000', '4000', '8000', '10000', '20000','30000']
+train_set = ['30000']
 n_val = 1000
 n_test = 10000
 op = sys.argv[1]
